8afea8be17cf0f6440cf3276df5e39f1f681fb45
Julien Dané <julien.Dane@gmail.com>
2024-01-07 12:18:34 +0100

    Refactor code for better app structure

---
+++ part4/lecture/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Give password as an argument.");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.0c1nx.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note({
+//   content: "CSS is hard",
+//   important: true,
+// });
+
+// note.save().then((result) => {
+//   console.log("Note saved.", result);
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => console.log(note));
+  mongoose.connection.close();
+});
