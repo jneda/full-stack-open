7120b79ae94e78f6908944b5b2ae89f719e28692
Julien Dané <julien.Dane@gmail.com>
2024-01-09 16:04:00 +0100

    Refactor Blog component and update CSS styles

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div className="blog">
-    {blog.title}<span className="author"> - {blog.author}</span>
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const detailsVisibile = { display: showDetails ? "" : "none" };
+
+  const toggleDetails = () => setShowDetails(!showDetails);
+
+  return (
+    <div className="blog">
+      <div className="blogEntry">
+        {blog.title}
+        <span className="author"> - {blog.author}</span>
+        <button onClick={toggleDetails}>{showDetails ? "Hide" : "View"}</button>
+      </div>
+      <div className="blogDetails" style={detailsVisibile}>
+        <span>{blog.url}</span>
+        <div className="like">
+          <span>Likes: {blog.likes}</span>
+          <button>Like</button>
+        </div>
+        <span>{blog.user.name}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
