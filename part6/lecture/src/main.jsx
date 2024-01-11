fcc82ccc4e922ce55caa008b315aee7abb171c6c
Julien Dané <julien.Dane@gmail.com>
2024-01-11 17:25:54 +0100

    Add new notes and initialize notes in App component

--- part6/lecture/src/main.jsx
+++ part6/lecture/src/main.jsx
@@ -1,16 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import store from "./store";
 
 import App from "./App";
 import "./index.css";
-
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const store = configureStore({
-  reducer: { notes: noteReducer, filter: filterReducer },
-});
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
