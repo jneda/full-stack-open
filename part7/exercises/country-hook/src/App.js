540e75c44f0ce1d9f054a88376d60e75b857e463
Julien Dané <julien.Dane@gmail.com>
2024-01-12 15:27:10 +0100

    Add country-hook files

---
+++ part7/exercises/country-hook/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = "https://studies.cs.helsinki.fi/restcountries/api/name";
+    const fetchCountry = async (name) => {
+      try {
+        const response = await axios.get(`${apiUrl}/${name}`);
+        console.log(response.data);
+        setCountry({ ...response.data, found: true });
+      } catch (error) {
+        if (error.status === 404) setCountry({ found: false });
+      }
+    };
+
+    if (name && name !== "") {
+      fetchCountry(name);
+    }
+  }, [name]);
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null;
+  }
+
+  if (!country.found) {
+    return <div>not found...</div>;
+  }
+
+  return (
+    <div>
+      <h3>{country.name.offical} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div>
+      <img
+        src={country.flags.png}
+        height="100"
+        alt={`flag of ${country.name.offical}`}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
