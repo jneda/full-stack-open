edaac3e98318ab3b5bb08f7890be24328d4e7bae
Julien Dané <julien.Dane@gmail.com>
2024-01-12 10:05:52 +0100

    Update votes for anecdotes

--- part6/exercises/anecdotes-react-query/src/App.jsx
+++ part6/exercises/anecdotes-react-query/src/App.jsx
@@ -1,20 +1,36 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAll } from "./requests";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAll, updateAnecdote } from "./requests";
 
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(["anecdotes"]);
+      queryClient.setQueryData(
+        ["anecdotes"],
+        anecdotes.map((a) =>
+          a.id !== updatedAnecdote.id ? a : updatedAnecdote
+        )
+      );
+    },
+  });
+
   const handleVote = (anecdote) => {
-    console.log("vote");
+    updateAnecdoteMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    });
   };
 
   const queryResult = useQuery({
     queryKey: ["anecdotes"],
     queryFn: getAll,
   });
-
-  console.log(JSON.parse(JSON.stringify(queryResult)));
 
   if (queryResult.isLoading) {
     return <div>Loading...</div>;
