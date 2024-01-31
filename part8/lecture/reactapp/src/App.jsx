135f8dca465599736eebe5059685fb78da7f6cc6
Julien Dané <julien.Dane@gmail.com>
2024-01-31 11:38:14 +0100

    Add login functionality and logout button Set authorization header Update cache

--- part8/lecture/reactapp/src/App.jsx
+++ part8/lecture/reactapp/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useApolloClient } from "@apollo/client";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 import { ALL_PERSONS } from "./queries";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
+  const client = useApolloClient();
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
@@ -22,9 +25,26 @@ const App = () => {
     }, 5000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    );
+  }
+
   return (
     <>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>Logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
