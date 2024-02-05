f836771c2c5172fe1a051e6591068a761c8839aa
Julien Dané <julien.Dane@gmail.com>
2024-02-05 17:53:04 +0100

    Refactor exercise calculator and add exercise endpoint

--- part9/exercises/first-steps/exerciseCalculator.ts
+++ part9/exercises/first-steps/exerciseCalculator.ts
@@ -8,87 +8,44 @@ interface Result {
   average: number;
 }
 
-interface ExerciseValues {
-  target: number;
-  exerciseHours: number[];
-}
-
-const exerciseCalculator = () => {
-  const parseArguments = (args: string[]): ExerciseValues => {
-    if (args.length < 4) throw new Error("Not enough arguments");
-
-    const [targetValue, ...rest] = args.slice(2);
-
-    const target = Number(targetValue);
-    if (isNaN(target)) {
-      throw new Error(`Value "${targetValue}" is not a number`);
+const getRatingDescription = (rating: number) => {
+  switch (rating) {
+    case 1: {
+      return "insufficient - more effort is needed";
     }
-
-    const exerciseHours: number[] = [];
-    for (const value of rest) {
-      const hours = Number(value);
-      if (isNaN(hours)) {
-        throw new Error(`Value "${value}" is not a number`);
-      }
-      exerciseHours.push(hours);
+    case 2: {
+      return "not too bad but could be better";
     }
-
-    return { target, exerciseHours };
-  };
-
-  const getRatingDescription = (rating: number) => {
-    switch (rating) {
-      case 1: {
-        return "insufficient - more effort is needed";
-      }
-      case 2: {
-        return "not too bad but could be better";
-      }
-      case 3: {
-        return "excellent - you reached the goal";
-      }
-      default: {
-        throw new Error("Unexepected rating value");
-      }
+    case 3: {
+      return "excellent - you reached the goal";
     }
-  };
-
-  const calculateExercises = (
-    exerciseHours: number[],
-    target: number
-  ): Result => {
-    const periodLength = exerciseHours.length;
-    const trainingDays = exerciseHours.filter((h) => h > 0).length;
-
-    const average =
-      exerciseHours.reduce((total, exercise) => total + exercise) /
-      periodLength;
-    const success = average >= target;
-    const rating = success ? 3 : average >= target * 0.75 ? 2 : 1;
-
-    const ratingDescription = getRatingDescription(rating);
-
-    return {
-      periodLength,
-      trainingDays,
-      success,
-      rating,
-      ratingDescription,
-      target,
-      average,
-    };
-  };
-
-  try {
-    const { target, exerciseHours } = parseArguments(process.argv);
-    console.log(calculateExercises(exerciseHours, target));
-  } catch (error: unknown) {
-    let errorMessage = "An error occurred.";
-    if (error instanceof Error) {
-      errorMessage = errorMessage.concat(` Error: ${error.message}`);
+    default: {
+      throw new Error("Unexepected rating value");
     }
-    console.error(errorMessage);
   }
 };
 
-exerciseCalculator();
+export const calculateExercises = (
+  exerciseHours: number[],
+  target: number
+): Result => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter((h) => h > 0).length;
+
+  const average =
+    exerciseHours.reduce((total, exercise) => total + exercise) / periodLength;
+  const success = average >= target;
+  const rating = success ? 3 : average >= target * 0.75 ? 2 : 1;
+
+  const ratingDescription = getRatingDescription(rating);
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
--- part9/exercises/first-steps/index.ts
+++ part9/exercises/first-steps/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import qs from "qs";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
 app.set("query parser", (str: string) => qs.parse(str));
+app.use(express.json());
 
 app.get("/hello", (_req, res) => res.send("Hello Full Stack!"));
 
@@ -31,6 +33,30 @@ app.get("/bmi", (req, res) => {
   }
 });
 
-const PORT = 3000;
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (!daily_exercises || !target) {
+    return res.status(400).send({ error: "parameters missing" });
+  }
+
+  if (
+    typeof target !== "number" ||
+    !Array.isArray(daily_exercises) ||
+    daily_exercises.some((value) => typeof value !== "number")
+  ) {
+    return res.status(400).send({ error: "malformatted parameters" });
+  }
+
+  return res.send(
+    calculateExercises(
+      daily_exercises.map((v) => Number(v)),
+      Number(target)
+    )
+  );
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
