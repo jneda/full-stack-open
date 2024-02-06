ef4dc5b548134686581faa076119c6d8c71c0dc1
Julien Dané <julien.Dane@gmail.com>
2024-02-06 17:48:46 +0100

    Add cors package and configure middleware

--- part9/lecture/flight-diary/src/index.ts
+++ part9/lecture/flight-diary/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import cors from "cors";
 import diaryRouter from "./routes/diaries";
 
 const app = express();
-app.use(express.json());
+app.use(cors(), express.json());
 
 const PORT = 3000;
 
