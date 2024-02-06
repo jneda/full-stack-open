b25b01ff7431e241b543e10f3957245dd64c2c39
Julien Dané <julien.Dane@gmail.com>
2024-02-06 12:30:55 +0100

    Refactor code to use utility function for creating new diary entries

--- part9/lecture/flight-diary/src/services/diaryService.ts
+++ part9/lecture/flight-diary/src/services/diaryService.ts
@@ -1,6 +1,6 @@
 import diaryData from "../../data/diaries";
 
-import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
+import { DiaryEntry, NonSensitiveDiaryEntry, NewDiaryEntry } from "../types";
 
 const diaries: DiaryEntry[] = diaryData;
 
@@ -15,10 +15,23 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  return diaries.find((d) => d.id === id);
+};
 
 export default {
   getEntries,
   addDiary,
   getNonSensitiveEntries,
+  findById,
 };
