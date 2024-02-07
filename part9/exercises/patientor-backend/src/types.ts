785a8841fba893fc3cf50b5e66f66146b829e6eb
Julien Dané <julien.Dane@gmail.com>
2024-02-07 16:13:21 +0100

    Refactor patient data structure and add entry types

--- part9/exercises/patientor-backend/src/types.ts
+++ part9/exercises/patientor-backend/src/types.ts
@@ -10,7 +10,47 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type Patient = {
   id: string;
